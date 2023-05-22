c165b604dfcaa597f1f3ff610281294cfebee3a5
Antoine Van Uffelen <antoine@mansio.be>
2023-05-22 18:28:26 +0200

    Initial

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useStripe } from '@stripe/stripe-react-native';
 
-export default function App() {
+// Replace with your own Stripe publishable key
+const PUBLISHABLE_KEY = 'pk_test_your_publishable_key';
+
+// Replace with your server endpoint for creating payments
+const PAYMENT_ENDPOINT = 'https://your-server-endpoint.com/payments';
+
+const HomeScreen = ({ navigation }) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initializePaymentSheet = async () => {
+      try {
+        const { error } = await initPaymentSheet({
+          paymentIntentClientSecret: 'your_payment_intent_client_secret',
+        });
+
+        if (!error) {
+          setLoading(false);
+        }
+      } catch (e) {
+        console.log('Error initializing payment sheet:', e);
+      }
+    };
+
+    initializePaymentSheet();
+  }, []);
+
+  const handlePayment = async () => {
+    try {
+      const { error } = await presentPaymentSheet();
+
+      if (error) {
+        console.log('Payment failed:', error);
+      } else {
+        console.log('Payment succeeded!');
+      }
+    } catch (e) {
+      console.log('Error during payment:', e);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 24, marginBottom: 20 }}>Stripe Integration Example</Text>
+      <Button title="Make Payment" onPress={handlePayment} disabled={loading} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
